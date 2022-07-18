b13a8712fe27ae91a446511f24a49a108e5f8c9e
mustardseed-web <mustardseed184@gmail.com>
2022-07-18 13:43:08 +0900

    画面調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,43 +9,55 @@
   <title>Document</title>
 </head>
 
-<body>
-  <h1>
-    Todo List
-  </h1>
-  <div>
-    <form action="{{ route('create') }}" method="post">
-      @csrf
-      @error('title')
-      <p>{{$message}}</p>
-      @enderror
-      <input type="text" name="title">
-      <button type="submit">追加</button>
-    </form>
-    <table>
-      <tbody>
-        <tr>
-          <td>作成日</td>
-          <td>タスク名</td>
-          <td>更新</td>
-          <td>削除</td>
-        </tr>
-      </tbody>
-    </table>
+<body class="bgcolor">
+  <div class="container">
+    <h1 class="title">
+      Todo List
+    </h1>
     <div>
-      @foreach($todos as $todo)
-      <details>
-        {{$todo->created_at}}
-        {{$todo->title}}
+      <form action="{{ route('create') }}" method="post">
         @csrf
+        @error('title')
+        <p>{{$message}}</p>
+        @enderror
+        <input type="text" name="title" size="50" class="text">
+        <button type="submit" class="add_btn">追加</button>
+      </form>
+      <table class="table">
+        <tbody>
+          <tr>
+            <div>
+              <td class="column_created_at">作成日</td>
+            </div>
+            <div>
+              <td class="column_title">タスク名</td>
+            </div>
+            <div>
+              <td class="column_updated_at">更新</td>
+            </div>
+            <div>
+              <td class="column_delete">削除</td>
+            </div>
+          </tr>
+        </tbody>
+      </table>
+      <div class="display_flex">
+        @foreach($todos as $todo)
+        <div class="todo-created_at">
+          {{$todo->created_at}}
+        </div>
+        <div class="todo-title">
+          <input type="text" name="title" size="50" class="#">
+          {{$todo->title}}
+        </div>
         <button type="submit">更新</button>
         <form action="{{ route('delete', ['todoId' => $todo->id]) }}" method="post">
           @method('DELETE')
           @csrf
           <button type="submit">削除</button>
         </form>
-      </details>
-      @endforeach
+        @endforeach
+      </div>
     </div>
   </div>
 </body>

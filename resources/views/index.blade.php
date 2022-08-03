3d171e0bcc4041ae84d525e3a626327750ae6a84
mustardseed-web <mustardseed184@gmail.com>
2022-08-03 12:35:39 +0900

    エラー

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -66,9 +66,9 @@
               {{-- カテゴリー選択 --}}
               <select class="form-control" id="category-id" name="category_id">
                 @foreach ($categories as $category)
-                {{-- <option value="{{ $category->id }}">{{ $category->category_name }}</option> --}}
-                <option value="{{ $category->id }}" @if(old('category_id')==$category->id) selected @endif>{{
-                  $category->category_name }}</option>
+                <option value="{{ $category->id }}">{{ $todo->category->category_name }}</option>
+                {{-- <option value="{{ $category->id }}" @if(old('category_id')==$category->id) selected @endif>{{
+                  $category->category_name }}</option> --}}
                 @endforeach
               </select>
 

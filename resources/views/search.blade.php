ffdfe050f619e5f539b26b83bdb541afc29daad4
mustardseed-web <mustardseed184@gmail.com>
2022-08-07 18:16:33 +0900

    CSSが聞かない

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -41,61 +41,62 @@
         {{-- 検索ボタン --}}
         <input type="submit" value="検索" class="serach_button_sample">
       </form>
+    </div>
 
 
-      <div class="todo_list_container">
-        <table>
-          <tr>
-            <th class="th_created_at">作成日</th>
-            <th class="th_task_name">タスク名</th>
-            <th class="th_category">タグ</th>
-            <th class="th_updated_at">更新</th>
-            <th class="th_delete">削除</th>
-          </tr>
-      </div>
+    <div class="todo_list_container">
+      <table>
+        <tr>
+          <th class="th_created_at">作成日</th>
+          <th class="th_task_name">タスク名</th>
+          <th class="th_category">タグ</th>
+          <th class="th_updated_at">更新</th>
+          <th class="th_delete">削除</th>
+        </tr>
+      </table>
+    </div>
 
-      <!--検索結果テーブル 検索された時のみ表示する-->
-      @if (!empty($posts))
-      <div class="#">
-        <table class="#">
-          @foreach($posts as $post)
-          <tr>
-            <td>{{ $post->created_at }}</td>
-            <td>
-              <form action="{{ route('searchUpdate', ['postId' => $post->id]) }}" method="post">
-                @method('PUT')
-                @csrf
-                <!--  更新内容入力欄 -->
-                <input type="text" name="title" size="50" value="{{$post->title}}" class="#">
-                <!--  カテゴリープルダウン -->
-                <select class="form-control" id="category_id" name="category_id">
-                  @foreach ($categories as $category)
-                  {{-- $category->idはカテゴリーテーブルのid --}}
-                  {{-- $post->category->idは検索結果のレコードに紐づくカテゴリーid --}}
-                  <option value="{{ $category->id }}" @if($category->id==$post->category->id) selected @endif>{{
-                    $category->category_name }}</option>
-                  @endforeach
-                </select>
+    <!--検索結果テーブル 検索された時のみ表示する-->
+    @if (!empty($posts))
+    <div class="#">
+      <table class="#">
+        @foreach($posts as $post)
+        <tr>
+          <td>{{ $post->created_at }}</td>
+          <td>
+            <form action="{{ route('searchUpdate', ['postId' => $post->id]) }}" method="post">
+              @method('PUT')
+              @csrf
+              <!--  更新内容入力欄 -->
+              <input type="text" name="title" size="50" value="{{$post->title}}" class="#">
+              <!--  カテゴリープルダウン -->
+              <select class="form-control" id="category_id" name="category_id">
+                @foreach ($categories as $category)
+                {{-- $category->idはカテゴリーテーブルのid --}}
+                {{-- $post->category->idは検索結果のレコードに紐づくカテゴリーid --}}
+                <option value="{{ $category->id }}" @if($category->id==$post->category->id) selected @endif>{{
+                  $category->category_name }}</option>
                 @endforeach
-                <!--  更新ボタン -->
-                <button type="submit" class="update_btn">更新</button>
-              </form>
-            <td class="column_delete">
-              {{-- 削除機能 --}}
-              <form action="{{ route('searchDelete', ['postId' => $post->id]) }}" method="post">
-                @method('DELETE')
-                @csrf
-                <!--  削除ボタン -->
-                <button type="submit" class="delete_btn">削除</button>
-              </form>
-            </td>
-          </tr>
-        </table>
-        {{-- @else
-        <p>見つかりませんでした。</p> --}}
-        @endif
-        <a href="{{ url('/') }}">戻る</a>
-      </div>
+              </select>
+              @endforeach
+              <!--  更新ボタン -->
+              <button type="submit" class="update_btn">更新</button>
+            </form>
+          </td>
+          <td class="column_delete">
+            {{-- 削除機能 --}}
+            <form action="{{ route('searchDelete', ['postId' => $post->id]) }}" method="post">
+              @method('DELETE')
+              @csrf
+              <!--  削除ボタン -->
+              <button type="submit" class="delete_btn">削除</button>
+            </form>
+          </td>
+        </tr>
+      </table>
+      @endif
+      <a href="{{ url('/') }}">戻る</a>
+    </div>
 
 </body>
 

f981167213fa667b78d037f30e4d611305ec842a
mustardseed-web <mustardseed184@gmail.com>
2022-08-07 16:31:23 +0900

    戻るボタン

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,31 +11,28 @@
 
 <body class="bgcolor">
   <div class="container">
+    <div class="header_title_button">
+      <h1 class="title">
+        Todo List
+      </h1>
+      <form action="{{ route('logout') }}" method="post" class="logput_button_position">
+        @csrf
+        <span>「{{ auth()->user()->name }}」でログイン中</span>
+        <input class="lg_btn" type="submit" value="ログアウト">
+      </form>
+    </div>
+    <button class="atag_search_button" type="button" onclick="location.href='{{ route('search_index') }}' ">タスク検索</button>
 
-    {{-- @if (Auth::check())
-    <p>ログイン中ユーザー: {{$user->name . ' メール' . $user->email . ''}}</p>
-    @else --}}
-    {{-- <p>ログインしてください。（<a href="/login">ログイン</a>｜
-      <a href="/register">登録</a>）
-    </p>
-    @endif --}}
-
-
-    <h1 class="title">
-      Todo List
-    </h1>
-    <button type="button" onclick="location.href='{{ route('search_index') }}' ">タスク検索</button>
-    <div>
-
+    <div class="sub_container">
       <form action="{{ route('create') }}" method="post">
         @csrf
         @error('title')
         <p>{{$message}}</p>
         @enderror
         {{-- todo入力欄 --}}
-        <input type="text" name="title" size="90" class="text">
+        <input type="text" name="title" size="100" class="text">
         <!--  カテゴリープルダウン -->
-        <select class="form-control" id="category-id" name="category_id">
+        <select class="add_category" id="category-id" name="category_id">
           @foreach ($categories as $category)
           <option value="{{ $category->id }}">{{ $category->category_name }}</option>
           @endforeach
@@ -44,48 +41,55 @@
         <button type="submit" class="add_btn">追加</button>
       </form>
 
-      <table>
-        <tr>
-          <th class="column_created_at">作成日</th>
-          <th class="column_created_at">タスク名</th>
-          <th class="column_title">タグ</th>
-          <th class="column_updated_at">更新</th>
-          <th class="column_delete">削除</th>
-        </tr>
+      <div class="todo_list_container">
+        <table>
+          <tr>
+            <th class="th_created_at">作成日</th>
+            <th class="th_task_name">タスク名</th>
+            <th class="th_category">タグ</th>
+            <th class="th_updated_at">更新</th>
+            <th class="th_delete">削除</th>
+          </tr>
+      </div>
 
-        @foreach($todos as $todo)
-        <tr>
-          <th class="column_created_at">{{$todo->created_at}}</th>
+      @foreach($todos as $todo)
+      <tr class="todo_list">
+        <td class="column_created_at">{{$todo->created_at}}</td>
+        {{-- 更新ボタン --}}
+        <form action="{{ route('update', ['todoId' => $todo->id]) }}" method="post">
+          @method('PUT')
+          @csrf
+          <td>
+            {{-- タスク入力欄 --}}
+            <input type="text" name="title" size="30" value="{{$todo->title}}" class="todo_task_name">
+          </td>
 
-          {{-- 更新ボタン --}}
-          <th class="column_updated_at">
-            <form action="{{ route('update', ['todoId' => $todo->id]) }}" method="post">
-              @method('PUT')
-              @csrf
-              <input type="text" name="title" size="50" value="{{$todo->title}}" class="#">
-              {{-- カテゴリー選択 --}}
-              <select class="form-control" id="category-id" name="category_id">
-                @foreach ($categories as $category)
-                {{-- <option value="{{ $category->id }}">{{ $todo->category->category_name }}</option> --}}
-                <option value="{{ $category->id }}" @if($category->id==$todo->category->id) selected @endif>{{
-                  $category->category_name }}</option>
-                @endforeach
-              </select>
+          <td>
+            {{-- カテゴリー選択 --}}
+            <select class="column_category" id="category-id" name="category_id">
+              @foreach ($categories as $category)
+              <option value="{{ $category->id }}" @if($category->id==$todo->category->id) selected @endif>{{
+                $category->category_name }}</option>
+              @endforeach
+            </select>
+          </td>
 
-              <button type="submit" class="update_btn">更新</button>
-            </form>
-          </th>
-          {{-- 削除ボタン --}}
-          <th class="column_delete">
-            <form action="{{ route('delete', ['todoId' => $todo->id]) }}" method="post">
-              @method('DELETE')
-              @csrf
-              <button type="submit" class="delete_btn">削除</button>
-            </form>
-          </th>
-        </tr>
-
-        @endforeach
+          <td>
+            {{-- 更新ボタン --}}
+            <button type="submit" class="update_btn">更新</button>
+          </td>
+        </form>
+        {{-- </td> --}}
+        {{-- 削除ボタン --}}
+        <td class="column_delete">
+          <form action="{{ route('delete', ['todoId' => $todo->id]) }}" method="post">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="delete_btn">削除</button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
       </table>
     </div>
   </div>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,81 +9,94 @@
   <title>Document</title>
 </head>
 
-<body>
-  <h1 class="title">
-    タスク検索
-  </h1>
-  {{-- //* 検索機能ここから *// --}}
-  <form action="{{ route('search') }}" method="get">
-    @csrf
-    <!--入力欄-->
-    <input type="text" name="searchWord">
-    <!--  カテゴリープルダウン -->
-    <div class="form-group">
-      <select class="form-control" id="category_id" name="category_id">
-        @foreach ($categories as $category)
-        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-        {{-- <option value="{{ $category->id }}" @if($category->id==$todo->category->id) selected @endif>{{ --}}
-          $category->category_name }}</option>
-        @endforeach
-      </select>
+<body class="bgcolor">
+  <div class="container">
+    <div class="header_title_button">
+      <h1 class="title">
+        タスク検索
+      </h1>
+      <form action="{{ route('logout') }}" method="post" class="logput_button_position">
+        @csrf
+        <span>「{{ auth()->user()->name }}」でログイン中</span>
+        <input class="lg_btn" type="submit" value="ログアウト">
+      </form>
     </div>
-    {{-- 検索ボタン --}}
-    <input type="submit" value="検索">
-  </form>
 
-  {{-- 検索結果項目 --}}
-  <table>
-    <tr>
-      <th class="column_created_at">作成日</th>
-      <th class="column_created_at">タスク名</th>
-      <th class="column_title">タグ</th>
-      <th class="column_updated_at">更新</th>
-      <th class="column_delete">削除</th>
-    </tr>
-  </table>
-
-  <!--検索結果テーブル 検索された時のみ表示する-->
-  @if (!empty($posts))
-  <div class="#">
-    <table class="#">
-      @foreach($posts as $post)
-      <tr>
-        <td>{{ $post->created_at }}</td>
-        <td>
-          <form action="{{ route('searchUpdate', ['postId' => $post->id]) }}" method="post">
-            @method('PUT')
-            @csrf
-            <!--  更新内容入力欄 -->
-            <input type="text" name="title" size="50" value="{{$post->title}}" class="#">
-            <!--  カテゴリープルダウン -->
-            <select class="form-control" id="category_id" name="category_id">
-              @foreach ($categories as $category)
-              {{-- <option value="{{ $category->id }}">{{ $category->category_name }}</option> --}}
-              <option value="{{ $category->id }}" @if($category->id==$todo->category->id) selected @endif>{{
-                $category->category_name }}</option>
-              @endforeach
-            </select>
+    <div class="sub_container">
+      {{-- //* 検索機能ここから *// --}}
+      <form action="{{ route('search') }}" method="get" class="display_flex">
+        @csrf
+        <!--入力欄-->
+        <input type="text" name="searchWord" size="100" class="search_textbox">
+        <!--  カテゴリープルダウン -->
+        <div class="form-group">
+          <select class="search_category" id="category_id" name="category_id">
+            @foreach ($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+            {{-- <option value="{{ $category->id }}" @if($category->id==$todo->category->id) selected @endif>{{ --}}
+              $category->category_name }}</option>
             @endforeach
-            <!--  更新ボタン -->
-            <button type="submit" class="update_btn">更新</button>
-          </form>
-        <td class="column_delete">
-          {{-- 削除機能 --}}
-          <form action="{{ route('searchDelete', ['postId' => $post->id]) }}" method="post">
-            @method('DELETE')
-            @csrf
-            <!--  削除ボタン -->
-            <button type="submit" class="delete_btn">削除</button>
-          </form>
-        </td>
-      </tr>
-    </table>
-    {{-- @else
-    <p>見つかりませんでした。</p> --}}
-    @endif
-  </div>
-  <a href="{{ url('/') }}">戻る</a>
+          </select>
+        </div>
+        {{-- 検索ボタン --}}
+        <input type="submit" value="検索" class="serach_button_sample">
+      </form>
+
+
+      <div class="todo_list_container">
+        <table>
+          <tr>
+            <th class="th_created_at">作成日</th>
+            <th class="th_task_name">タスク名</th>
+            <th class="th_category">タグ</th>
+            <th class="th_updated_at">更新</th>
+            <th class="th_delete">削除</th>
+          </tr>
+      </div>
+
+      <!--検索結果テーブル 検索された時のみ表示する-->
+      @if (!empty($posts))
+      <div class="#">
+        <table class="#">
+          @foreach($posts as $post)
+          <tr>
+            <td>{{ $post->created_at }}</td>
+            <td>
+              <form action="{{ route('searchUpdate', ['postId' => $post->id]) }}" method="post">
+                @method('PUT')
+                @csrf
+                <!--  更新内容入力欄 -->
+                <input type="text" name="title" size="50" value="{{$post->title}}" class="#">
+                <!--  カテゴリープルダウン -->
+                <select class="form-control" id="category_id" name="category_id">
+                  @foreach ($categories as $category)
+                  {{-- $category->idはカテゴリーテーブルのid --}}
+                  {{-- $post->category->idは検索結果のレコードに紐づくカテゴリーid --}}
+                  <option value="{{ $category->id }}" @if($category->id==$post->category->id) selected @endif>{{
+                    $category->category_name }}</option>
+                  @endforeach
+                </select>
+                @endforeach
+                <!--  更新ボタン -->
+                <button type="submit" class="update_btn">更新</button>
+              </form>
+            <td class="column_delete">
+              {{-- 削除機能 --}}
+              <form action="{{ route('searchDelete', ['postId' => $post->id]) }}" method="post">
+                @method('DELETE')
+                @csrf
+                <!--  削除ボタン -->
+                <button type="submit" class="delete_btn">削除</button>
+              </form>
+            </td>
+          </tr>
+        </table>
+        {{-- @else
+        <p>見つかりませんでした。</p> --}}
+        @endif
+        <a href="{{ url('/') }}">戻る</a>
+      </div>
+
 </body>
 
 </html>
